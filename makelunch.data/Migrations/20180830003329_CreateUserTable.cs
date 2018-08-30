679214083ee30ec2604d82e70e5f6bfa54a49326
brharper-clgx <bryanbharper@gmail.com>
2018-08-29 19:34:58 -0500

    WIP: Fix: Create User

---
+++ makelunch.data/Migrations/20180830003329_CreateUserTable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace makelunch.data.Migrations
+{
+    public partial class CreateUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "fee_worksheets",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    name = table.Column<string>(nullable: true),
+                    avatar_url = table.Column<string>(nullable: true),
+                    hard_no_restaurant_ids = table.Column<List<string>>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_fee_worksheets", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fee_worksheets");
+        }
+    }
+}
